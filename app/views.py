c50b6fdf115e010d124a2a7279306d26902f81e3
Martin H. Bramwell <martinhbramwell@gmail.com>
2013-06-09 12:47:00 +0000

    Created a new page (appl.html) for data management. Removed old trial version (tryIt.html)

--- app/views.py
+++ app/views.py
@@ -127,6 +127,17 @@ def user(nickname, page = 1):
         user = user,
         posts = posts)
 
+@app.route('/appl')
+@app.route('/appl/<int:page>')
+@login_required
+def appl(page = 1):
+    # user = User.query.filter_by(nickname = nickname).first()
+    # if user == None:
+    #     flash(gettext('User %(nickname)s not found.', nickname = nickname))
+    #     return redirect(url_for('index'))
+    return render_template('appl.html',
+        user = current_user)
+
 @app.route('/edit', methods = ['GET', 'POST'])
 @login_required
 def edit():
