a17827c1cbf8ae92e5daf3db661051daa80659fc
M. H. J. Bramwell <mhb.warehouseman@gmail.com>
2013-07-08 06:26:32 -0400

    Get real data from lease table
     On branch master
     Changes to be committed:
    
    	modified:   app/templates/lease.html
    	modified:   app/templates/recLease.html
    	modified:   app/templates/stake.html
    	modified:   app/templates/tank.html
    	modified:   app/templates/well.html
    	modified:   app/view/vwLease.py

--- app/view/vwLease.py
+++ app/view/vwLease.py
@@ -14,11 +14,9 @@ from app.forms.fmLease import LeaseForm
 @app.route('/lease/<nickname>', methods = ['GET', 'POST'])
 @app.route('/lease/<nickname>/<int:page>', methods = ['GET', 'POST'])
 @login_required
-def lease(page = 1, nickname = 'Ooops!  Bad'):
-	aLease = Lease.query.get(1)
-	unique = aLease.nick_name
+def lease(nickname=None, page = 1):
+	leases = Lease.query.filter() # .paginate(page, POSTS_PER_PAGE, False)
 	form = LeaseForm()
-	a = ['spork', 'eggs', 100, 1234, 9999]
-	pyld = {'key': unique, 'page': 'Lease', 'form': form}
-	return render_template('lease.html', user = current_user, payload = pyld, posts = a)
+	pyld = {'page': 'Lease', 'key': 'Baby', 'form': form, 'records': leases}
+	return render_template('lease.html', payload = pyld)
 
